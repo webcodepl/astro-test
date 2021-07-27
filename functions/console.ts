15c1459cbbfeae5d5adc477a22b02825f7d2f4fe
Tomasz Pupiec <tomasz.pupiec@netrix.com.pl>
2021-07-27 15:21:30 +0200

    stringify context

--- functions/console.ts
+++ functions/console.ts
@@ -1,10 +1,10 @@
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
 	const subject = event.queryStringParameters.name || "World";
 
 	const test: string = "test";
 
 	return {
 		statusCode: 200,
-		body: JSON.stringify(event),
+		body: JSON.stringify(context),
 	};
 };
